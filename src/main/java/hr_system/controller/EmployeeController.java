6c0c8e9300629c2613b7620857783a19f9b2cb21
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-07-13 13:12:23 +0300

    changning to rest

--- src/main/java/hr_system/controller/EmployeeController.java
+++ src/main/java/hr_system/controller/EmployeeController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import hr_system.entity.AppUser;
 import hr_system.entity.Tasks;
 import hr_system.service.IEmployeeService;
 
-@Controller
+@RestController
 public class EmployeeController {
 	
 	private IEmployeeService employeeService;
@@ -21,6 +22,11 @@ public class EmployeeController {
 		super();
 		this.employeeService = employeeService;
 	}
+	@GetMapping("")
+	public String hello() {
+		return "Helloword";
+	}
+	
 	
 	@GetMapping("/employees/tasks/{id}")
 	public List<Tasks> listTasks(@PathVariable Integer id) {
